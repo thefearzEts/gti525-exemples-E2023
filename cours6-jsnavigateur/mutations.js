9f3f2875f9f38a16bfdb1220a71af883491eb26f
Julien Gascon-Samson <julien.gascon-samson@etsmtl.ca>
2023-06-12 19:18:10 -0400

    Mutations sans solution

--- cours6-jsnavigateur/mutations.js
+++ cours6-jsnavigateur/mutations.js
@@ -5,8 +5,8 @@ Permet de modifier la couleur du texte (via la propriété style (CSS)) en la re
 function colorElements(tag, color) {
     const elements = document.getElementById("contents").getElementsByTagName(tag);
     for (let i=0; i<elements.length; i++) {
-	const elt = elements[i];
-	elt.style.color = color;
+        const elt = elements[i];
+        elt.style.color = color;
     }
 }
 
@@ -14,38 +14,10 @@ function colorElements(tag, color) {
 Permet d'observer les noeuds pour lesquels la couleur est modifiée pour prendre la valeur oldColor (mutations à la propriété style), qui sont descendants du noeud dont l'id=contents. Vous devez remplacer la couleur par newColor. Cette fonction doit être implémentée au moyen d'un observateur de mutations.
 */
 function trapColorChanges(oldColor, newColor) {
-    const callback = function(mutationList, observer) {
-	for (const mutation of mutationList) {
-	    if (mutation.type == "attributes" && mutation.attributeName == "style") {
-		// Vérifier que la couleur est oldColor
-		if (mutation.target.style.color == oldColor) {
-		    setTimeout( () => mutation.target.style.color = newColor, 5000);
-		}
-	    }
-	}
-    }
     
-    const observer = new MutationObserver(callback);
-
-    const targetNode = document.getElementById("contents");
-
-    const config = { attributes: true, subtree: true };
-
-    observer.observe(targetNode, config);
 }
 
 window.onload = function() {
 
-    /* 1. Définir un timer qui va invoquer colorElements afin de remplacer, après 5 secondes, la couleur de tous les noeuds (à l'intérieur de contents):
-       • span en rouge (red)
-       • p en bleu (blue)
-    */
-    setTimeout( () => {
-	colorElements("span", "red");
-	colorElements("p", "blue");
-    }, 5000);
     
-    // 2. Invoquer trapColorChanges afin d'intercepter les noeuds dont la couleur change en rouge (red), afin de plutôt remplacer la couleur par vert (green)
-    // ...
-    trapColorChanges("red", "green");
 }
