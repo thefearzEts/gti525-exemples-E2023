a771882b38b32c7e8e135696669ea6ecc688ac51
Julien Gascon-Samson <julien.gascon-samson@etsmtl.ca>
2023-07-10 18:23:56 -0400

    Corrigé bogue - handleErrors et non handleErrors(response)

--- cours8-ajax/httpbin.js
+++ cours8-ajax/httpbin.js
@@ -5,7 +5,7 @@ function handleErrors(response) {
 }
 
 fetch("https://httpbin.org/post", {method: "POST", body: "GTI525"})
-    .then( handleErrors( response ) )
+    .then( handleErrors )
     .then( response => response.json() )
     .then( data => console.log( data.data) )
     .catch( error => console.log( "Une erreur est survenue: " + error ));
