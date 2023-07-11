5489b5db21eb2215e5f8f22091f6e17cfdfd61b8
Julien Gascon-Samson <julien.gascon-samson@etsmtl.ca>
2023-07-10 21:43:57 -0400

    Fix bug importation avec ./ pour modules locaux

--- cours9-nodejs/modules_import.js
+++ cours9-nodejs/modules_import.js
@@ -1,12 +1,16 @@
 // Importer le module module Calculator
-var calculator = require("module_calculator.js");
+var calculator = require("./module_calculator.js");
 calculator.sum(10, 20);
 
 // Importer le module Shapes
-var Point = require("module_shapes.js");
+var Point = require("./module_shapes.js");
 var p = new Point(1, 2);
 
+// Importer le module Shapes
+var Point2 = require("./module_shapes2.js");
+var p = new Point2(1, 2);
+
 // Importer le module Counter
-var counter = require("module_counter.js");
+var Counter = require("./module_counter.js");
 var counter = Counter(5);
 counter.increment();  counter.reset(); counter.increment();
