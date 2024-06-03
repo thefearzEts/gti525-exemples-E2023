48e88482546483352cfdab73241de9cea33ff04e
Julien Gascon-Samson <julien.gascon-samson@etsmtl.ca>
2024-06-03 18:47:48 -0400

    Amélioration exemple sur timers

--- cours6-jsnavigateur/example.js
+++ cours6-jsnavigateur/example.js
@@ -1,18 +1,56 @@
-var myVar;
+// Exemple de setTimeout avec passage de paramètre
 
-function setupTimeout(i) {
-	
-	return	function() {
-		alert("Hello " + i);
+var timeoutHandler1 = function(message) {
+	alert(message);
+};
+
+var ret = setTimeout(timeoutHandler1, 100, "Hello")
+
+// .....
+
+// Exemple de setTimeout avec fonction englobante
+
+var timeoutHandler2 = function(message) {
+	alert(message);
+};
+
+var ret = setTimeout(function() { timeoutHandler2("Hello") }, 100);
+
+// .....
+
+// Exemple de setTimeout avec fonction fléchée englobante
+
+var timeoutHandler3 = function(message) {
+	alert(message);
+};
+
+var ret = setTimeout(() => timeoutHandler3("Hello"), 100);
+
+// .....
+
+// Exemple de setTimeout avec fermeture
+
+var timeoutHandler4 = function(message) {
+	return function() {
+		alert(message);
 	}
+};
 
-}
+var ret = setTimeout(timeoutHandler4("Hello"), 100)
 
-function cancelTimeout() {
-	i = 0;
-	clearTimeout(myVar);
-}
+// .....
 
-for (var k=0; k<10; k++) {
-	myVar = setTimeout(setupTimeout(k), 3000);
-}
+// Exemple de setInterval avec fermeture
+
+var intervalHandler = function(message) {
+	var i = 0;
+	return function() {
+		console.log(message + '  ' + i);  
+		i += 1;
+
+
+	}
+};
+var ret = setInterval(intervalHandler("invocation"), 1000);
+
+setTimeout( () => clearInterval(ret), 5000)
